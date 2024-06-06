eaafb981998c017ee124514d223bd53ca4dcacbd
Jomar Rumbawa <jomargrumbawa@gmail.com>
2024-06-05 19:09:00 -0700

    Add event listeners to buttons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-let firstNum
-let secondNum
-let operator
+const numButtons = document.querySelectorAll(".num--button")
+const eqlButton = document.querySelector(".eql--button")
+const clrButton = document.querySelector(".clr--button")
+const operButton = document.querySelector(".oper--button")
+const calcDisplay = document.querySelector(".calc--display")
+let firstNum;
+let secondNum;
+let operator;
+let display;
 
 function add(a, b) {
   return a + b
@@ -29,3 +35,17 @@ function operate(firstNum, secondNum, operator) {
     return divide(firstNum , secondNum)
   }
 }
+
+numButtons.forEach(function(button) {
+  button.addEventListener("click", function(e) {
+    if (calcDisplay.textContent === "0") {
+      calcDisplay.textContent = e.target.textContent
+    } else {
+      calcDisplay.textContent += e.target.textContent
+    }
+  })
+})
+
+clrButton.addEventListener("click", function() {
+  calcDisplay.textContent = 0
+})
